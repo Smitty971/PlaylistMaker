c8809d269523cec44089dd27bb566a68a4ab5b2e
Jamal Turner <jlsmitty2273@yahoo.com>
2020-08-11 08:29:08 -0400

    rename files and class

--- js-frontend/src/adapters/ListAdapter.js
+++ js-frontend/src/adapters/ListAdapter.js
@@ -1,10 +1,9 @@
-class ListsAdapter {
+class ListAdapter {
     constructor(){
         this.baseUrl = 'http://localhost:3000/lists'
     }
 
     getLists() {
-        return fetch(this.baseUrl)
-        .then(res =>.json())
+        return fetch(this.baseUrl).then(res =>res.json())
     }
 }
